65b08b78a3590baa6f1d9e61673987d44ad8bf00
nishant-project <nishantkumardas0@gmail.com>
2025-10-30 15:06:17 +0530

    initial commit

---
+++ src/test/java/com/surest/surest_member_management/SurestMemberManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surest.surest_member_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurestMemberManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
